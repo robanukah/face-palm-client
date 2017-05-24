5535fc36a574f57e2780b4a94af3c1a0727c9e3b
solairerove <hax2033@gmail.com>
2017-05-24 15:54:33 +0300

    Old style http service

--- src/app/post-data.service.ts
+++ src/app/post-data.service.ts
@@ -1,20 +1,35 @@
 import {Injectable} from '@angular/core';
 import {Post} from './post';
+import {Observable} from 'rxjs/Observable';
+import {Http} from '@angular/http';
 
 @Injectable()
 export class PostDataService {
 
-  posts: Post[] = [];
-
-  constructor() {
+  constructor(private http: Http) {
   }
 
-  createPost(post: Post): PostDataService {
-    this.posts.push(post);
-    return this;
+  fetchPosts(): Observable<Post[]> {
+    return this.http
+      .get('/api/posts')
+      .map(this.extractData)
+      .catch(this.handleError);
   }
 
-  getAllPosts(): Post[] {
-    return this.posts;
+  private extractData(res: Response) {
+    return res.json() || {};
+  }
+
+  private handleError(error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
   }
 }
