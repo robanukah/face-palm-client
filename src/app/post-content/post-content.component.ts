eccabbde1e8c6fdebfef6e805d419bc3e1c2bbc2
solairerove <hax2033@gmail.com>
2017-05-29 12:05:19 +0300

    Add angular route support

--- src/app/post-content/post-content.component.ts
+++ src/app/post-content/post-content.component.ts
@@ -1,6 +1,7 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PostDataService} from '../post-data.service';
 import {Post} from '../post';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-post-content',
@@ -10,13 +11,20 @@ import {Post} from '../post';
 export class PostContentComponent implements OnInit {
 
   post: Post;
+  id: string;
 
-  constructor(private service: PostDataService) {
+  constructor(private service: PostDataService,
+              private route: ActivatedRoute) {
   }
 
   ngOnInit() {
+
+    this.route
+      .params
+      .subscribe(params => this.id = params['id']);
+
     this.service
-      .fetchPost("dvdf")
+      .fetchPost(this.id)
       .subscribe(post => this.post = post);
   }
 }
