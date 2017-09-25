a3352fe468ec61c51247e2942fa57d03557dfe7d
solairerove <hax2033@gmail.com>
2017-09-25 22:15:44 +0300

    feat: add axios dependency

---
+++ src/components/ExampleComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="example">
+        <ul v-if="posts && posts.length">
+            <li v-for="post in posts" v-bind:key="post.id">
+                <p>
+                    <strong>{{post.title}}</strong>
+                </p>
+                <p>{{post.body}}</p>
+            </li>
+        </ul>
+
+        <ul v-if="errors && errors.length">
+            <li v-for="error in errors" v-bind:key="error">
+                {{error.message}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+// import axios from 'axios';
+
+export default {
+    el: 'example',
+    data: () => ({
+        posts: [],
+        errors: []
+    })
+}
+</script>
